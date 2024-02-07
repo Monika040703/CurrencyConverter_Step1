60d6b86f4856aed5b9cd6d8ec6fcf927ea3cfa5c
Tanaz Imrankhan Pathan <path0051@algonquinlive.com>
2024-02-07 17:00:16 -0500

    modified rate

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -23,7 +23,7 @@ import android.widget.TextView;
 */
 
 public class MainActivity extends AppCompatActivity {
-    static private final Float CONVERSION_RATE = 0.80F;
+    static private final Float CONVERSION_RATE = 1.00F;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
